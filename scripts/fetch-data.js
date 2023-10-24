caf7a2b33b2e2a9d2558a088e482a14ab06fc15a
Shahbaj Mukul <shahbajmukul@gmail.com>
2023-10-24 00:32:20 -0400

    working dropdown

--- scripts/fetch-data.js
+++ scripts/fetch-data.js
@@ -1,19 +1,37 @@
-function fetchData(theSpan) {
+const currencyArray = [];
+function fetchData() {
     fetch('https://api.coincap.io/v2/assets')
-        .then(Response => Response.json()).then(json => {
-            let content = "";
-            json.forEach(currency => {
-                content += currency.id;
-            });
+        .then(Response => Response.json())
+        .then(json => {
+            for (const currency of json.data) {
+                currencyArray.push(currency);
+            }
+            console.log(currencyArray);
+            populateDropdown();
+
         })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+fetchData();
+
+function populateDropdown() {
+    const dropdown = document.getElementById('currencyList');
+
+    for (const currency of currencyArray) {
+        const option = document.createElement('option');
+
+        option.value = currency.id;
+        option.text = currency.name;
     
+        dropdown.appendChild(option);
+    }
 }
 
 
+function dropDownFunction() {
+    var currencies = document.getElementById("currencyList");
 
-
-function hideOrShow() {
-    document.getElementById("")
 }
-
-export { hideOrShow}
