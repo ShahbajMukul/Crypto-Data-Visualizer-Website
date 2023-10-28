b51ba8cdc5ad536d0eb9180babb61d0ba2c964ef
Shahbaj Mukul <shahbajmukul@gmail.com>
2023-10-27 22:23:04 -0400

    final project

--- scripts/fetch-data.js
+++ scripts/fetch-data.js
@@ -1,37 +1,53 @@
 const currencyArray = [];
-function fetchData() {
-    fetch('https://api.coincap.io/v2/assets')
-        .then(Response => Response.json())
-        .then(json => {
-            for (const currency of json.data) {
-                currencyArray.push(currency);
-            }
-            console.log(currencyArray);
-            populateDropdown();
 
+function fetchCryptoData() {
+    fetch('https://api.coincap.io/v2/assets')
+        .then(response => response.json())
+        .then(json => {
+            currencyArray.push(...json.data);
+            console.log(currencyArray);
+            
+            populateDropdown();
         })
         .catch(error => {
             console.error('Error fetching data:', error);
+            showError("Error getting data from the API. Please try again later.");
         });
 }
 
-fetchData();
-
 function populateDropdown() {
     const dropdown = document.getElementById('currencyList');
-
     for (const currency of currencyArray) {
         const option = document.createElement('option');
-
         option.value = currency.id;
         option.text = currency.name;
-    
         dropdown.appendChild(option);
     }
 }
 
-
-function dropDownFunction() {
-    var currencies = document.getElementById("currencyList");
-
+function updateInfo(selectedId) {
+    const selectedCurrency = currencyArray.find(currency => currency.id === selectedId);
+    if (selectedCurrency) {
+        document.getElementById('cryptoName').textContent = selectedCurrency.name;
+        document.getElementById('cryptoSymbol').textContent = `Symbol: ${selectedCurrency.symbol}`;
+        document.getElementById('cryptoSupply').textContent = `Supply: ${Math.round(selectedCurrency.supply)} units`;
+        document.getElementById('cryptoPrice').textContent = `Price in USD: $${parseFloat(selectedCurrency.priceUsd).toFixed(2)}`;
+        document.getElementById('cryptoChange').textContent = `Change in 24hr: ${parseFloat(selectedCurrency.changePercent24Hr).toFixed(2)}%`;
+        document.getElementById('cryptoRank').textContent = `Rank: ${selectedCurrency.rank}`;
+    } else {
+        showError("Couldn't display information for the selected currency. Please try again.");
+    }
 }
+
+function dropdown() {
+    const selectElement = document.getElementById('currencyList');
+    const selectValue = selectElement.value;
+    updateInfo(selectValue);
+}
+
+function showError(message) {
+    const cryptoInfoSection = document.getElementById('cryptoInfo');
+    cryptoInfoSection.innerHTML = `<p>${message}</p>`;
+}
+
+fetchCryptoData();
